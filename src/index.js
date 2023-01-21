e5311aa783529edc8f0e0618630c47ca7372a742
Vika <viktoria.wout@gmail.com>
2023-01-21 14:02:53 +0100

    MARVEL API authorized, fetching func created using classes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './components/app/App';
+import MarvelService from './services/MarvelService';
+import './style/style.scss';
 
+const marvelService = new MarvelService();
+marvelService.getAllCharacters().then(res => res.data.results.map(item => console.log(item.name)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,5 +13,4 @@ root.render(
     <App />
   
 );
-
 
