b1b11b2cf2356ba09059f078ac4537d90c6d7908
Vika <viktoria.wout@gmail.com>
2023-02-04 14:00:04 +0100

    focusOnItem method added to complete styles to focusing the element using ref

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -12,7 +12,7 @@ class CharList extends Component {
         loading: true,
         error: false,
         newItemLoading: false,
-        offset: 1548,
+        offset: 210,
         charListEnded: false
     }
 
@@ -56,21 +56,46 @@ class CharList extends Component {
         })
     }
 
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
     renderItems(arr) {
-        const items = arr.map((item) => {
+        const items = arr.map((item, i) => {
             let imgStyle = { 'objectFit': 'cover' };
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = { 'objectFit': 'unset' };
             }
 
+            const active_style = ({isActive}) => isActive ? 'img_selected' : ''
+
             return (
-                <li
-                    className="char__item"
-                    key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
-                    <img src={item.thumbnail} alt={item.name} style={imgStyle} />
+                <li 
+                className="char__item"
+                tabIndex={0}
+                ref={this.setRef}
+                key={item.id}
+                onClick={() => {
+                    this.props.onCharSelected(item.id);
+                    this.focusOnItem(i);
+                }}
+                onKeyPress={(e) => {
+                    if (e.key === ' ' || e.key === "Enter") {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }
+                }}>
+                    <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                     <div className="char__name">{item.name}</div>
-                </li>
+            </li>
             )
         });
 
