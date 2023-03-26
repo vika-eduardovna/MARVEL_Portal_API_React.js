70c03a99bbc5973b21e83e93069eacc8575d6e3d
Vika <viktoria.wout@gmail.com>
2023-03-26 12:20:13 +0200

    fixed bug with loading content in CharList component

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -14,11 +14,11 @@ const CharList = (props) => {
     const { loading, error, getAllCharacters } = useMarvelService();
 
     useEffect(() => {
-        onRequest();
+        onRequest(offset, true);
     }, [])
 
-    const onRequest = (offset) => {
-        setNewItemLoading(true)
+    const onRequest = (offset, initial) => {
+        initial ? setNewItemLoading(false) : setNewItemLoading(true)
         getAllCharacters(offset)
             .then(onCharListLoaded)
     }
@@ -80,14 +80,14 @@ const CharList = (props) => {
     const items = renderItems(charList);
 
     const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error) ? items : null;
+    const spinner = loading && !newItemLoading ? <Spinner /> : null;
+    
 
     return (
         <div className="char__list">
             {errorMessage}
             {spinner}
-            {content}
+            {items}
             <button
                 className="button button__main button__long"
                 disabled={newItemLoading}
